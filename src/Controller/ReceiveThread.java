bc851cefb2e7ca10804bba5baeb3b4352d0200b8
Dinh Khanh <dinhkhanh412@gmail.com>
2020-06-09 12:50:07 +0700

    fix gr chat

--- src/Controller/ReceiveThread.java
+++ src/Controller/ReceiveThread.java
@@ -169,6 +169,7 @@ public class ReceiveThread implements Runnable {
 				return;
 			}
 			if (msg.getCommand().equals("GROUP")) {
+				chatUIController.receiveMess(message.getBody());
 				System.out.println("[" + message.getSender() + " to " + message.getReceiver() + "]: " + message.getBody());
 				return;
 			}
